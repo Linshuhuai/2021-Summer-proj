2b49f39b63fb820559963a329aa5ba91e2da7d3d
Linshuhuai <68037461+Linshuhuai@users.noreply.github.com>
2021-07-26 23:59:24 +0800

    Add files via upload

---
+++ Unity/制作我的背包/InventoryManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    static InventoryManager instance;
+
+    public Inventory myBag;
+    public GameObject slotGrid;
+    public Slot slotPrefab;
+    public Text itemInformation;
+
+    void Awake(){
+        if(instance != null)
+            Destroy(this);
+        instance = this; 
+    }
+
+    private void OnEnable(){
+        RefreshItem();
+        instance.itemInformation.text = ""; 
+    }
+
+    public static void UpdateItemInfo(string itemDescription)
+    {
+        instance.itemInformation.text = itemDescription;
+    }
+
+    public static void CreateNewItem(Item item)
+    {
+        //place new item in grid
+        Slot newItem = Instantiate(instance.slotPrefab, instance.slotGrid.transform.position, Quaternion.identity);
+        newItem.gameObject.transform.SetParent(instance.slotGrid.transform);
+
+        //transform information
+        newItem.slotItem = item;
+        newItem.slotImage.sprite = item.itemImage;
+        // newItem.slotNum.text = item.itemHeld.ToString();
+
+
+    }
+
+    public static void RefreshItem()
+    {
+        for(int i = 0; i < instance.slotGrid.transform.childCount; i++)
+        {
+            Destroy(instance.slotGrid.transform.GetChild(0).gameObject);
+        }
+        
+        foreach (Item item in instance.myBag.itemList)
+        {
+            CreateNewItem(item);
+        }
+        
+    }
+
+}
